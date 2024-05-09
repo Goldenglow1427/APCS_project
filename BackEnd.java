d567bc5b2d023f21c52b4cbe4492f9940d40daf0
Goldenglow1427 <magician1427@gmail.com>
2024-05-09 14:16:28 -0400

    Writing backend interface

---
+++ BackEnd.java
@@ -0,0 +1,14 @@
+public interface BackEnd
+{
+    /**
+     * Based on the current time, return an array containing all the blocks away.
+     * @return An array of all the blocks away.
+     */
+    public int[] getPixelArray();
+
+    /**
+     * Assume the time has flied by "dt" miliseconds. Change the getPixelArray.
+     * @param dt The amount of time need to be changed.
+     */
+    public void moveTime(int dt);
+}
